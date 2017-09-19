d83ff13de1e6f245555fcbd07d6e0f73b3d2228b
houzhiwei <1575790064@qq.com>
2017-09-19 17:21:06 +0800

    init.

---
+++ DemoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoTest
+//
+//  Created by 候志伟 on 2017/9/19.
+//  Copyright © 2017年 bocweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DemoTest
+//
+//  Created by 候志伟 on 2017/9/19.
+//  Copyright © 2017年 bocweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
